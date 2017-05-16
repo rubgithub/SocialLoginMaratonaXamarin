23fcef39d8dfc908dbf22d0267ae71652c03de65
rubgithub <rubensatual@gmail.com>
2017-05-15 23:14:01 -0400

    teste commit

--- SocialLoginMaratona/SocialLoginMaratona/Service/AzureService.cs
+++ SocialLoginMaratona/SocialLoginMaratona/Service/AzureService.cs
@@ -6,7 +6,7 @@ namespace SocialLoginMaratona.Service
 {
     public class AzureService
     {
-        //TODO: refatorar de acordo cm PDF
+        //TODO: refatorar de acordo cm PDFs
         static readonly string AppUrl = "https://urlparabackend.zurewebsite.net";
         public MobileServiceClient Client { get; set; } = null;
 
